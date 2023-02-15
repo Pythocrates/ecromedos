18696a21468818d26a3106b9b08c15981a4fcff4
Pythocrates <23015037+Pythocrates@users.noreply.github.com>
2023-02-15 01:11:41 +0100

    Reformat another build script.

--- src/ecromedos/style/make.sh
+++ src/ecromedos/style/make.sh
@@ -4,33 +4,29 @@
 # Functions
 ##############################################################################
 
-do_clean()
-{
+do_clean() {
     rm -fr css
     rm -f style.css
 }
 
-do_build()
-{
+do_build() {
     do_clean
     mkdir -p css
 
-    ls scss/ | while read scss_file
-    do
+    ls scss/ | while read scss_file; do
         # continue if not a regular file
         test -f scss/$scss_file || continue
 
         sass -C --scss --style expanded scss/$scss_file \
             --precision 8 \
-            >> css/`basename $scss_file .scss`.css
+            >>css/$(basename $scss_file .scss).css
     done
 
     rm -f css/variables.css
     rm -f style.css
 
-    for css_file in normalize grid layout style
-    do
-        cat "css/${css_file}.css" >> style.css
+    for css_file in normalize grid layout style; do
+        cat "css/${css_file}.css" >>style.css
     done
 }
 
@@ -39,12 +35,11 @@ do_build()
 ##############################################################################
 
 case "$1" in
-    clean)
-        do_clean
-        ;;
-    *)
-        do_clean
-        do_build
-        ;;
+clean)
+    do_clean
+    ;;
+*)
+    do_clean
+    do_build
+    ;;
 esac
-
